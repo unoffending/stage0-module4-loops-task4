8bedef9a1c6b39ab778980ce52fc140dcab52a29
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 09:32:38 +0600

    Update SkippingThird.java

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+    int i = 0;
+            while(i < lastPrinted) {
+                i++;
+                if(i % 3 == 0) {
+                    continue;
+                }
+                System.out.println(i);
+            }
     }
 }
